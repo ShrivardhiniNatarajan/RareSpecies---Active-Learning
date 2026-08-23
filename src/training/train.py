26676ee7c845ea8af39ff0512eb0caeca7037a0c
ShrivardhiniNatarajan <shrivardhininatarajan@gmail.com>
2026-08-23 18:12:43 +0530

    feat:changes to train.py

--- src/training/train.py
+++ src/training/train.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import models
 
-from src.training.dataset import (
+from dataset import (
     CameraTrapDataset,
     train_transform,
     eval_transform,
